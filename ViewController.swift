d35d2727f67742c2db3219562ca9d4ad113e6de5
Asad Asees <asadahmed@nyu.edu>
2014-11-13 23:22:55 -0500

    SATHelp

---
+++ ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Signs
+//
+//  Created by Asees on 11/3/14.
+//  Copyright (c) 2014 Asees. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let isLoggedIn:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        if (isLoggedIn != 1) {
+            self.performSegueWithIdentifier("goto_login", sender: self)
+        } else {
+            self.usernameLabel.text = prefs.valueForKey("USERNAME") as NSString
+        }
+    }
+
+    @IBAction func logoutTapped(sender: AnyObject) {
+        let appDomain = NSBundle.mainBundle().bundleIdentifier
+        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
+        
+        self.performSegueWithIdentifier("goto_login", sender: self)
+    }
+
+}
+
