5b496a1de56e28ab6955972b798fb450264a8e56
Asad Asees <asadahmed@nyu.edu>
2014-11-18 18:04:33 -0500

    Sprint 3
    Includes Facebook code and Swipe functionality

---
+++ Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  Signs
+//
+//  Created by Asees on 11/16/14.
+//  Copyright (c) 2014 Asees. All rights reserved.
+//
+
+#ifndef Signs_Bridge_Header_h
+#define Signs_Bridge_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
--- ViewController.swift
+++ ViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FBLoginViewDelegate {
+    @IBOutlet var fbLoginView : FBLoginView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var swipeLeft = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+    }
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Right:
+                println("Word will be saved")
+            case UISwipeGestureRecognizerDirection.Down:
+                println("Next Word is to be shown")
+            default:
+                break
+            }
+        }
     }
 
     @IBOutlet weak var usernameLabel: UILabel!
@@ -31,6 +55,23 @@ class ViewController: UIViewController {
             self.usernameLabel.text = prefs.valueForKey("USERNAME") as NSString
         }
     }
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+        println("This is where you perform a segue.")
+    }
+    
+    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser){
+        println("User Name: \(user.name)")
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
+    }
+
 
     @IBAction func logoutTapped(sender: AnyObject) {
         let appDomain = NSBundle.mainBundle().bundleIdentifier
